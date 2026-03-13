8691c97fd69b7d87616c5d5cb10f5061a8f2658f
felipe-rodriguesz <feliperodrigues.2004@alunos.utfpr.edu.br>
2026-03-13 19:23:02 -0300

    gráficos atualizados

--- scripts/run_instability.sh
+++ scripts/run_instability.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cd "$(dirname "$0")/.."
 
 echo "==================================================="
@@ -7,9 +6,14 @@ echo "         INICIANDO ESTUDO DE ESTABILIDADE          "
 echo "==================================================="
 
 echo "[..] Compilando os programas..."
-gfortran -Wall -O2 src/ftcs.f90 -o sim_ftcs
-gfortran -Wall -O2 src/btcs.f90 -o sim_btcs
+gfortran -Wall -O2 src/ftcs.f90           -o sim_ftcs
+gfortran -Wall -O2 src/btcs.f90           -o sim_btcs
 gfortran -Wall -O2 src/crank_nicolson.f90 -o sim_cn
+
+if [ $? -ne 0 ]; then
+    echo "[ERRO] Falha na compilacao."
+    exit 1
+fi
 
 MALHA=50
 FATORES=(1.0 1.02)
@@ -18,13 +22,13 @@ for F in "${FATORES[@]}"; do
     echo "==================================================="
     echo " Testando Fator de Passo de Tempo (dt) = $F"
 
-    echo "$MALHA $MALHA $F" | ./sim_ftcs > /dev/null
+    echo "$MALHA $MALHA $F 1.0e8 0.010 0.010 0.5" | ./sim_ftcs > /dev/null
     mv serie_tempo_ftcs.dat data/results/stab_ftcs_f$F.dat 2>/dev/null
 
-    echo "$MALHA $MALHA $F" | ./sim_btcs > /dev/null
+    echo "$MALHA $MALHA $F 1.0e8 0.010 0.010 0.5" | ./sim_btcs > /dev/null
     mv serie_tempo_btcs.dat data/results/stab_btcs_f$F.dat 2>/dev/null
 
-    echo "$MALHA $MALHA $F" | ./sim_cn > /dev/null
+    echo "$MALHA $MALHA $F 1.0e8 0.010 0.010 0.5" | ./sim_cn > /dev/null
     mv serie_tempo_cn.dat data/results/stab_cn_f$F.dat 2>/dev/null
 done
 
--- scripts/run_results.sh
+++ scripts/run_results.sh
@@ -5,16 +5,20 @@ echo "==================================================="
 echo " GERANDO DADOS PARA A TABELA DO RELATÓRIO          "
 echo "==================================================="
 
-gfortran -Wall -O2 src/ftcs.f90 -o sim_ftcs
-gfortran -Wall -O2 src/btcs.f90 -o sim_btcs
+gfortran -Wall -O2 src/ftcs.f90           -o sim_ftcs
+gfortran -Wall -O2 src/btcs.f90           -o sim_btcs
 gfortran -Wall -O2 src/crank_nicolson.f90 -o sim_cn
 
-# Roda os 3 métodos na malha 100x100 COM o fator_dt = 1.0
-echo "100 100 1.0" | ./sim_ftcs > /dev/null
-echo "100 100 1.0" | ./sim_btcs > /dev/null
-echo "100 100 1.0" | ./sim_cn > /dev/null
+if [ $? -ne 0 ]; then
+    echo "[ERRO] Falha na compilacao."
+    exit 1
+fi
 
-# Move as métricas e logs para data/results/
+echo "100 100 1.0 1.0e8 0.010 0.010 0.5" | ./sim_ftcs > /dev/null
+echo "100 100 1.0 1.0e8 0.010 0.010 0.5" | ./sim_btcs > /dev/null
+echo "100 100 1.0 1.0e8 0.010 0.010 0.5" | ./sim_cn   > /dev/null
+
+# Move resultados para data/results/
 mv log_*.txt metricas_*.dat serie_tempo_*.dat snap_*.dat *.dat data/results/ 2>/dev/null
 rm -f sim_ftcs sim_btcs sim_cn
 
